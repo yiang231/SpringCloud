dcf40c1200f6d10eb22d6b88c6b9a50e2ea6d51b
Yiang <1712064922@qq.com>
2022-10-13 23:17:07 +0800

    实现了Eureka客户端以及服务端，修改springboot启动器版本为spring版本

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /*
 SpringBoot整合MyBatis
@@ -37,6 +38,7 @@ SpringBoot整合MyBatis
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.atguigu.springcloud.dao")
+@EnableEurekaClient
 public class PaymentMain8001 {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentMain8001.class, args);
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -2,10 +2,11 @@ package com.atguigu.springcloud.dao;
 
 import com.atguigu.springcloud.entity.Payment;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 //@Component       //代替@Repository声明bean
 //@Mapper               //mybatis提供的，等价：@MapperScan("com.atguigu.springcloud.dao")
-//@Repository     //spring提供的。在此，只是为了声明bean对象
+@Repository     //spring提供的。在此，只是为了声明bean对象
 public interface PaymentDao {
 	public int create(Payment payment);
 
