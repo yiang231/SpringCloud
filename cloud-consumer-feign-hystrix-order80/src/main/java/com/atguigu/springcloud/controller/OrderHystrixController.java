884ee8b497158f66e3eb171bddaa99db8a1812d9
Yiang <1712064922@qq.com>
2022-10-15 21:42:07 +0800

    实现了豪猪哥订单模块全局默认的兜底方法，出现异常时直接执行兜底方法

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -12,21 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+//全局默认的兜底方法
 @DefaultProperties(defaultFallback = "paymentGlobalFallbackMethod")
 public class OrderHystrixController {
 	@Autowired
 	private PaymentHystrixService paymentHystrixService;
 
+	@HystrixCommand
 	@GetMapping("/consumer/payment/hystrix/ok/{id}")
 	public String paymentInfoOK(@PathVariable("id") Integer id) {
-		//int i = 10/0;
+		int i = 10 / 0;
 		String result = paymentHystrixService.paymentInfoOK(id);
 		log.info("*******result:" + result);
 		return result;
 	}
 
-	@HystrixCommand(fallbackMethod = "paymentTimeoutFallback", commandProperties = {
-			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")})
+	@HystrixCommand(fallbackMethod = "paymentTimeoutFallback",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")})
 	@GetMapping("/consumer/payment/hystrix/timeout/{id}")
 	public String paymentInfoTimeout(@PathVariable("id") Integer id) {
 		String result = paymentHystrixService.paymentInfoTimeout(id);
@@ -37,5 +39,10 @@ public class OrderHystrixController {
 	//兜底方法，上面方法出问题,我来处理，返回一个出错信息
 	public String paymentTimeoutFallback(Integer id) {
 		return "特定托底方法：我是消费者80,对方支付系统繁忙稍后再试。或自己运行出错，请检查自己。";
+	}
+
+	//下面是全局fallback方法
+	public String paymentGlobalFallbackMethod() {
+		return "公共托底方法：Global异常处理信息，请稍后再试,(┬＿┬)";
 	}
 }
